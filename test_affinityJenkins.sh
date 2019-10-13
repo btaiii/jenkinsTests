cacf9fcf13794dfc61b9d0afe13ac815478da17d
Branch Archer <btaiii@yahoo.com>
2019-10-12 20:37:07 -0500

    new ommands

--- test_affinityJenkins.sh
+++ test_affinityJenkins.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # use bash shell
 echo hello
-# cd /home/branch/Dropbox/EE382V_AdvancedProgrammingTools/jenkins/affinityJenkins
+cd /home/branch/Dropbox/EE382V_AdvancedProgrammingTools/jenkins/affinityJenkins
+pwd
 # python3 -m nose --with-coverage --cover-package=affinityJenkins --cover-inclusive > testoutput 2>&1 
-# python3 -m nose --with-coverage --cover-package=affinityJenkins --cover-inclusive
+python3 -m nose --with-coverage --cover-package=affinityJenkins --cover-inclusive
