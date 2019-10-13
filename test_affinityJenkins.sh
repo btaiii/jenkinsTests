9b9309bebc84d5b8c398fb00c8895f6fc048fd6f
Branch Archer <btaiii@yahoo.com>
2019-10-12 20:19:01 -0500

    use bin/sh

--- test_affinityJenkins.sh
+++ test_affinityJenkins.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # use bash shell
 cd /home/branch/Dropbox/EE382V_AdvancedProgrammingTools/jenkins/affinityJenkins
-python3 -m nose --with-coverage --cover-package=affinityJenkins --cover-inclusive
+python3 -m nose --with-coverage --cover-package=affinityJenkins --cover-inclusive > testoutput 2>&1 
