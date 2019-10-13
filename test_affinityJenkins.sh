02930bc325617ed441cdb966cbea9fa4fc48a4df
Branch Archer <btaiii@yahoo.com>
2019-10-12 20:29:08 -0500

    whoami and echo

--- test_affinityJenkins.sh
+++ test_affinityJenkins.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # use bash shell
-cd /home/branch/Dropbox/EE382V_AdvancedProgrammingTools/jenkins/affinityJenkins
-python3 -m nose --with-coverage --cover-package=affinityJenkins --cover-inclusive > testoutput 2>&1 
+echo hello
+# cd /home/branch/Dropbox/EE382V_AdvancedProgrammingTools/jenkins/affinityJenkins
+# python3 -m nose --with-coverage --cover-package=affinityJenkins --cover-inclusive > testoutput 2>&1 
+# python3 -m nose --with-coverage --cover-package=affinityJenkins --cover-inclusive
