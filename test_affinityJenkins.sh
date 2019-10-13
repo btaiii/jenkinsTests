a004c35d1b0dff74b92a0ccb27256ca1ab6973f0
Branch Archer <btaiii@yahoo.com>
2019-10-12 20:05:27 -0500

    added #

--- test_affinityJenkins.sh
+++ test_affinityJenkins.sh
@@ -2,3 +2,4 @@
 # use bash shell
 cd /home/branch/Dropbox/EE382V_AdvancedProgrammingTools/jenkins/affinityJenkins
 python3 -m nose --with-coverage --cover-package=affinityJenkins --cover-inclusive &> testoutput
+#
