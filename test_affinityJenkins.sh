986554e27e205b0ee3bc6cc7186c3c345a2dc93d
Branch Archer <btaiii@yahoo.com>
2019-10-12 20:10:37 -0500

    no testoutput

--- test_affinityJenkins.sh
+++ test_affinityJenkins.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # use bash shell
 cd /home/branch/Dropbox/EE382V_AdvancedProgrammingTools/jenkins/affinityJenkins
-python3 -m nose --with-coverage --cover-package=affinityJenkins --cover-inclusive &> testoutput
+#
+python3 -m nose --with-coverage --cover-package=affinityJenkins --cover-inclusive 
+#python3 -m nose --with-coverage --cover-package=affinityJenkins --cover-inclusive &> testoutput
 #
