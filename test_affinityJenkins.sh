fbe922d853c3b6b40df16bdc4f2ea208006e6afc
Branch Archer <btaiii@yahoo.com>
2019-10-13 08:32:10 -0500

    final script

--- test_affinityJenkins.sh
+++ test_affinityJenkins.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-# use bash shell
-echo hello
+# note: Jenkins uses sh shell
+echo Current directory:
 cd /home/branch/Dropbox/EE382V_AdvancedProgrammingTools/jenkins/affinityJenkins
 pwd
 # python3 -m nose --with-coverage --cover-package=affinityJenkins --cover-inclusive > testoutput 2>&1 
-python3 -m nose --with-coverage --cover-package=affinityJenkins --cover-inclusive
+# python3 -m nose --with-coverage --cover-package=affinityJenkins --cover-inclusive
+PYTHONPATH=''
+python3 -m nose --with-xunit --all-modules --traverse-namespace --with-coverage --cover-package=affinityJenkins --cover-inclusive &> testoutput
+python3 -m coverage xml --include=affinityJenkins
+# can use parseable instead of colorized (works best with EMACS?)
+pylint -f colorized -d I0011,R0801 affinityJenkins | tee pylint.out
